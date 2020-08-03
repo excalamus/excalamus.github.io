dc40b72ce825eaf6a24cc6faa60ac5de3f1196ae
Excalamus <excalamus@tutanota.com>
2020-08-03 13:50:15 -0400

    Update with new defaults

--- config.el
+++ config.el
@@ -1,5 +1,35 @@
-(setq peut-publier-root-directory "~/projects/peut-publier-site/")
-(setq peut-publier-src-directory "~/projects/peut-publier-site/src/")
-(setq peut-publier-publish-directory "~/projects/peut-publier-site/publish/")
+(setq peut-publier-site-name "peut-publier demo")
+(setq peut-publier-root-directory "~/Projects/excalamus.github.io/")
+(setq peut-publier-src-directory
+      (concat peut-publier-root-directory "src/"))
+(setq peut-publier-publish-directory
+      (concat peut-publier-root-directory "publish/"))
 (setq peut-publier-about-img "https://thispersondoesnotexist.com/image")
 (setq peut-publier-about-img-alt "Headshot of a person who does not exist.")
+
+(setq peut-publier-body-preamble
+  (concat
+   "   <div id=\"preamble\" class=\"status\">\n"
+   "      <nav>\n"
+   "         <div class=\"flexcontainer\">\n"
+   "            <div class=\"smallitem\">\n"
+   "               <ul class=\"inline-list\">\n"
+   "                  <li><a href=\"index.html\" id=\"website\">" peut-publier-site-name "</a></li>\n"
+   "               </ul>\n"
+   "            </div>\n"
+   "            <div class=\"bigitem\">\n"
+   "               <ul class=\"inline-list\">\n"
+   "                  <li><a href=\"about.html\">About</a></li>\n"
+   "               </ul>\n"
+   "            </div>\n"
+   "         </div>\n"
+   "      </nav>\n"
+   "      <hr/>\n"
+   "   </div>\n"))
+
+(defun peut-publier-demo-html-page-title (page-path)
+  "Create HTML title for PAGE-PATH."
+  (concat
+   "      <title> peut-publier: " (peut-publier-alist-get "TITLE" (peut-publier-get-meta-data-alist page-path)) "</title>\n"))
+
+(setq peut-publier-variable-head #'peut-publier-demo-html-page-title)
